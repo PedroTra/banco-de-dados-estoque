28bf742286aff2282ec9dad34a48203b6be8281b
PedroTra <151466351+PedroTra@users.noreply.github.com>
2024-10-01 17:54:19 -0300

    Create class Venda.java

---
+++ class Venda.java
@@ -0,0 +1,104 @@
+package modelo.dominio;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Venda {
+
+    private long id;
+    private Cliente cliente;
+    private Usuario usuario;
+    private BigDecimal totalDaVenda;
+    private BigDecimal valorPago;
+    private BigDecimal descontoTotal;
+    private BigDecimal troco;
+    private LocalDateTime dataHoraCriacao;
+    private LocalDateTime ultimaAtualizacao;
+
+    public Venda() {
+    }
+
+    public Venda(long id, Cliente cliente, Usuario usuario, BigDecimal totalDaVenda, BigDecimal valorPago, BigDecimal descontoTotal, BigDecimal troco, LocalDateTime dataHoraCriacao, LocalDateTime ultimaAtualizacao) {
+        this.id = id;
+        this.cliente = cliente;
+        this.usuario = usuario;
+        this.totalDaVenda = totalDaVenda;
+        this.valorPago = valorPago;
+        this.descontoTotal = descontoTotal;
+        this.troco = troco;
+        this.dataHoraCriacao = dataHoraCriacao;
+        this.ultimaAtualizacao = ultimaAtualizacao;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public BigDecimal getTotalDaVenda() {
+        return totalDaVenda;
+    }
+
+    public void setTotalDaVenda(BigDecimal totalDaVenda) {
+        this.totalDaVenda = totalDaVenda;
+    }
+
+    public BigDecimal getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(BigDecimal valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public BigDecimal getDescontoTotal() {
+        return descontoTotal;
+    }
+
+    public void setDescontoTotal(BigDecimal descontoTotal) {
+        this.descontoTotal = descontoTotal;
+    }
+
+    public BigDecimal getTroco() {
+        return troco;
+    }
+
+    public void setTroco(BigDecimal troco) {
+        this.troco = troco;
+    }
+
+    public LocalDateTime getDataHoraCriacao() {
+        return dataHoraCriacao;
+    }
+
+    public void setDataHoraCriacao(LocalDateTime dataHoraCriacao) {
+        this.dataHoraCriacao = dataHoraCriacao;
+    }
+
+    public LocalDateTime getUltimaAtualizacao() {
+        return ultimaAtualizacao;
+    }
+
+    public void setUltimaAtualizaçao(LocalDateTime ultimaAtualizacao) {
+        this.ultimaAtualizacao = ultimaAtualizacao;
+    }
+}
