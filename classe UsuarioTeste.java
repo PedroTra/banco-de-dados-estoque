c4cb5c8c68a1011575cf282faab60f0fcab224d0
PedroTra <151466351+PedroTra@users.noreply.github.com>
2024-10-31 10:12:06 -0300

    Update classe UsuarioTeste.java

--- classe UsuarioTeste.java
+++ classe UsuarioTeste.java
@@ -11,6 +11,13 @@ public class UsuarioTeste {
         String mensagem = usuarioDado.salvar(usuario);
         System.out.println(mensagem);
 
+        
+        Usuario usuario = new Usuario(0L, "PedroPon", "1234", "Pedro02", Perfil.PADRAO, null, null);
+        UsuarioDado usuarioDado = new UsuarioDado();
+        String mensagem = usuarioDado.salvar(usuario);
+        System.out.println(mensagem);
+
+
 
     }
 }
